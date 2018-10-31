3c3e8cc548eb0d056d137d484c7cf5d6987a628f
Safuh <Safoh95@windowslive.com>
2018-10-31 06:24:57 -0700

    Spec half done

---
+++ W2D3/skeleton/spec/dessert_spec.rb
@@ -0,0 +1,68 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef", name: "Safuh") }
+  let(:cake) { Dessert.new("cake", 10, chef) }
+
+  describe "initialize" do
+    it "sets a type" do
+    expect(cake.type).to eq("cake")
+    end
+    it "sets a quantity" do
+      expect(cake.quantity).to eq(10)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(cake.ingredients.empty?).to be_truthy
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new('cake', 'alot', chef) }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      cake.add_ingredient('chocolate')
+      expect(cake.ingredients).to include("chocolate")
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      ingredients = ['chocolate', 'eggs', 'flour', 'sugar', 'butter']
+      ingredients.each do |ingret|
+        cake.add_ingredient(ingret)
+      end
+
+      expect(cake.ingredients).to eq(ingredients)
+      cake.mix!
+      expect(cake.ingredients).not_to eq(ingredients)
+      expect(cake.ingredients.sort).to eq(ingredients.sort)
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+      cake.eat(8)
+      expect(cake.quantity).to eq(2)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect{cake.eat(20).to raise_error("not enough left!")}
+  end
+end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name"
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in"
+  end
+end
